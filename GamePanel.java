3464215f7201f2c1e64efa58de545b9164939b0a
yonatankumsa <guitaristyona@gmail.com>
2023-01-25 12:18:09 -0500

    created instances for the apple and snake

--- GamePanel.java
+++ GamePanel.java
@@ -5,4 +5,21 @@ import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener{
 
+	static final int SCREEN_WIDTH = 1300;
+	static final int SCREEN_HEIGHT = 800;
+	static final int UNIT_SIZE = 50;
+	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE);
+	static final int DELAY = 79;
+	final int x[] = new int[GAME_UNITS];
+	final int y[] = new int[GAME_UNITS];
+	int bodyParts = 6;
+	int applesEaten;
+	int appleX;
+	int appleY;
+	char direction = 'R';
+	boolean running = false;
+	Timer timer;
+	Random random;
+	
+	
 }
