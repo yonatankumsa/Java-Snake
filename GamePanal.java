c056f033d782d41cd309b1a3da54535d9d926a7b
yonatankumsa <guitaristyona@gmail.com>
2023-01-25 02:00:04 -0500

    created classes

---
+++ GamePanal.java
@@ -0,0 +1,34 @@
+import javax.swing.JPanel;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+
+public class GamePanal extends JPanel implements AncestorListener{
+
+
+    GamePanal(){
+        
+    }
+
+
+
+
+
+    @Override
+    public void ancestorAdded(AncestorEvent event) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void ancestorRemoved(AncestorEvent event) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void ancestorMoved(AncestorEvent event) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
---
+++ SnakeGame.java
@@ -0,0 +1,11 @@
+import java.time.YearMonth;
+
+/**
+ * main
+ */
+public class SnakeGame {
+    public static void main(String[] args) {
+       new GameFrame();
+}
+    
+}
